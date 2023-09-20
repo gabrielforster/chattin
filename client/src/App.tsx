ef63f5be0eb3a0b7b1cf73e3486c22d16f3529e0
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-09-19 21:04:24 -0300

    feat: init frontend

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+import { Header } from "./components/Header.tsx"
+
+export default function App () {
+  return (
+    <div className="bg-zinc-800 text-white min-h-screen">
+      <Header />
+    </div>
+  )
+}
+
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+export function Header () {
+  return (
+    <div className="p-4 bg-zinc-800 text-white flex items-center justify-between">
+      <h1 className="text-4xl">Chattin</h1>
+      <nav>
+        <ul className="flex space-x-4">
+          <li>Icon</li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
