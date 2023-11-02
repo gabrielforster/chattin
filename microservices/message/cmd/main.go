eb91b3343bf67f1739e13b919a153fd0c9750cd6
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-11-02 00:52:39 -0300

    feat: adding pub/sub with redis to scale websocket servers

--- microservices/message/cmd/main.go
+++ microservices/message/cmd/main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
+	"os"
 	"net/http"
 
 	"github.com/gorilla/websocket"
+  "github.com/go-redis/redis/v8"
 )
+
+var configs = map[string]string{
+  // change to get it from env
+  "REDIS_ADDR": "localhost:6379",
+  "PORT": os.Args[1],
+}
 
 var upgrader = websocket.Upgrader{
   ReadBufferSize: 2048,
@@ -16,12 +25,44 @@ var upgrader = websocket.Upgrader{
 
 var conns []*websocket.Conn
 
+var ctx = context.Background()
+
+var redisPub = redis.NewClient(&redis.Options{
+  Addr: configs["REDIS_ADDR"],
+})
+
+var redisSub = redis.NewClient(&redis.Options{
+  Addr: configs["REDIS_ADDR"],
+})
+
 func main() {
-  http.HandleFunc("/", handler)
-  http.ListenAndServe(":42069", nil)
+  go func() {
+    sub := redisSub.Subscribe(ctx, "message")
+    ch := sub.Channel()
+    for msg := range ch {
+      handleMessageFromRedis(msg)
+    }
+  }()
+
+  http.HandleFunc("/", handlerWebsocketMessage)
+  http.ListenAndServe(":" + configs["PORT"], nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handleMessageFromRedis (message *redis.Message) {
+    for _, conn := range conns {
+      if err := conn.WriteMessage(1, []byte(message.Payload)); err != nil {
+        fmt.Println(err)
+        return
+      }
+    }
+}
+
+func handlerWebsocketMessage(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "GET" {
+    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
+
   socket, err := upgrader.Upgrade(w, r, nil)
   if err != nil {
     fmt.Println(err)
@@ -31,7 +72,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
   conns = append(conns, socket)
 
   for {
-    msgType, msg, err := socket.ReadMessage()
+    _, msg, err := socket.ReadMessage()
     if err != nil {
       fmt.Println(err)
       return
@@ -40,11 +81,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
     command, message := string(msg[:bytes.IndexByte(msg, ' ')]), string(msg[bytes.IndexByte(msg, ' ')+1:])
 
     if (command == "message") {
-      for _, conn := range conns {
-        if err := conn.WriteMessage(msgType, []byte(message)); err != nil {
-          fmt.Println(err)
-          return
-        }
+      if err := redisPub.Publish(ctx, "message", message).Err(); err != nil {
+        panic(err)
       }
     }
   }
